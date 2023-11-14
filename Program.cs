ebdd3b9b6da0609bdd0401f83d71f6030d3448f9
Jacob <jacobkile.mailbox@gmail.com>
2023-11-14 01:44:49 -0800

    Choice 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿﻿using NLog;
+using System.Linq;
+using NWConsole.Model;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 // See https://aka.ms/new-console-template for more information
 string path = Directory.GetCurrentDirectory() + "\\nlog.config";
@@ -7,6 +11,46 @@ string path = Directory.GetCurrentDirectory() + "\\nlog.config";
 var logger = LogManager.LoadConfiguration(path).GetCurrentClassLogger();
 logger.Info("Program started");
 
-Console.WriteLine("Hello World!");
+try
+{
+    var db = new NWContext();
+    string choice;
+    do
+    {
+    Console.WriteLine("1) Display Categories");
+        Console.WriteLine("2) Add Category");
+        Console.WriteLine("\"q\" to quit");
+        choice = Console.ReadLine();
+        Console.Clear();
+        logger.Info($"Option {choice} selected");
+        if (choice == "1")
+        {
+            var query = db.Categories.OrderBy(p => p.CategoryName);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{query.Count()} records returned");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            foreach (var item in query)
+            {
+                Console.WriteLine($"{item.CategoryName} - {item.Description}");
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        else if (choice == "2")
+        {
+            Category category = new Category();
+            Console.WriteLine("Enter Category Name:");
+            category.CategoryName = Console.ReadLine();
+            Console.WriteLine("Enter the Category Description:");
+            category.Description = Console.ReadLine();
+            // TODO: save category to db
+        }
+        Console.WriteLine();
+    } while (choice.ToLower() != "q");
+}
+catch (Exception ex)
+{
+    logger.Error(ex.Message);
+}
 
 logger.Info("Program ended");
